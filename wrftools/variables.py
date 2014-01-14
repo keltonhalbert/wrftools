5c9d209c144b7d1ff2671f3a543744ddbb3f620c
keltonhalbert <keltonhalbert@tempestchasing.com>
2014-01-14 17:34:54 -0600

    Added MAPFAC_M requirement to wrf_PV

--- wrftools/variables.py
+++ wrftools/variables.py
@@ -155,17 +155,18 @@ def wrf_absvort( U, V, F, dx ):
     assert U.shape == V.shape == F.shape, 'Arrays are different shapes. They must be the same shape.'
     return wrf_vort( U, V, dx ) + F
 
-def wrf_pv( U, V, F, THETA, PRES, dx ):
+def wrf_pv( U, V, F, THETA, PRES, MAPFAC_M, dx ):
     """Calculate the potential vorticity given the U and V vector components in m/s,
         the Coriolis sine latitude term (F) in s^-1, THETA potential temperature in degrees
-        Kelvin, PRES pressure in hPa or mb, and the gridspacing dx in meters. U, V, F, THETA,
-        and PRES must be 4D arrays.
+        Kelvin, PRES pressure in hPa or mb, the map scale factor on mass grid and the gridspacing
+        dx in meters. U, V, F, THETA, and PRES must be 4D arrays.
         ---------------------
         U (numpy.ndarray): ndarray of U vector values in m/s
         V (numpy.ndarray): ndarray of V vector values in m/s
         F (numpy.ndarray): ndarray of Coriolis sine latitude values in s^-1
         THETA (numpy.ndarray): ndarray of potential temperature in degrees Kelvin
         PRES (numpy.ndarray): ndarray of pressure in hPa or mb same shape as THETA
+        MAPFAC_M (numpy.ndarray): 2D of map scale factor on mass grid.
         dx (float or int): float or integer of U and V grispacing in meters
         ---------------------
         returns:
@@ -175,6 +176,7 @@ def wrf_pv( U, V, F, THETA, PRES, dx ):
     assert U.shape == V.shape == F.shape == THETA.shape == PRES.shape, 'Arrays are different shapes. They must be the same shape.'
     ## pres in hPa needs to convert to Pa
     PRES = PRES * 100
+    dx = dx * MAPFAC_M
     dy = dx
     grav = 9.8
     
