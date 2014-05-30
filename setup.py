0c21aa200b524a1322cac98a7d1f5b9666dbdcf0
keltonhalbert <keltonhalbert@tempestchasing.com>
2014-05-30 11:46:45 -0500

    updated packages in set.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.extension import Extension
 
 setup(
       name = "wrftools",
-      version = "1.0",
+      version = "1.5",
       author = "Kelton Halbert",
       author_email = "keltonhalbert@ou.edu",
       description = ("Functions for post processing WRF data"),
       license = "GPL V2",
       url = "https://github.com/keltonhalbert/wrftools",
-      packages=['wrftools'],
+      packages=['wrftools', 'wrftools.variables', 'wrftools.interp', 'wrftools.utils'],
       classifiers=["Development Status :: 3 - Alpha"]
       )
