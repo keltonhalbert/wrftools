c38d9487b2897119acf5941ccb92ebed1fc2727d
keltonhalbert <keltonhalbert@tempestchasing.com>
2014-06-04 12:03:02 -0500

    re-documented the function

--- wrftools/interp/pres.py
+++ wrftools/interp/pres.py
@@ -3,30 +3,44 @@
 ## http://tempestchasing.com
 ## Protected under the GPL V2 License
 
-import numpy
+import numpy as np
 
 def wrf_to_pres( grid, surface, interplevels ):
-    """Linearly interpolates a grid to various vertical levels defined by 
-        interplevels. This uses the numpy.interp function that requires
-        the function coordinate values to be monotonically increasing,
-        and this function does not check for this. See numpy.interp docs
-        for more information. Grid and surface must be the same shape.
-    -----------------------
-    grid (numpy.ndarray): 4D array of values to be interpolated onto a vertical surface.  Must be monotonically increasing.
-    surface (numpy.ndarray): 4D array of the verical coordinate values of the surface to be interpolated to. Must be monotonically increasing.
-    interplevels (numpy.ndarray): 1D array of vertical coordinate values that are desired to be interpolated to.
-    -----------------------
-    returns:
-        numpy.ndarray of values of shape (grid.shape[0], len( interplevels ), grid.shape[2], grid.shape[3] )
-    """
+    '''
+    Linearly interpolates a grid to interplevels. This uses the 
+    numpy.interp function that requires the function coordinate 
+    values to be monotonically increasing, and this function does 
+    not check for this. See numpy.interp docs for more information. 
+    Grid and surface must be the same shape.
+
+    Parameters
+    ----------
+    grid - 4D array of values to be interpolated onto a vertical surface.  
+        Must be monotonically increasing.
+    surface - 4D array of the verical coordinate values of the surface to be 
+        interpolated to. Must be monotonically increasing.
+    interplevels - 1D array of vertical coordinate values that are desired 
+        to be interpolated to.
+
+    Returns
+    -------
+    outgrid - numpy.ndarray of values of shape 
+        (grid.shape[0], len( interplevels ), grid.shape[2], grid.shape[3] )
+    '''
+    ## get the shape of the original grid but with the vertical dimension of
+    ## interplevels, and then allocate an empty array based on that shape.
     shape = ( grid.shape[0], interplevels.shape[0], grid.shape[2], grid.shape[3] )
-    outgrid = numpy.ones( shape )
-    for time in numpy.arange( grid.shape[0] ):
-        for idx, val in numpy.ndenumerate( grid[0][0] ):
+    outgrid = np.empty( shape )
+    ## loop through the time dimemsion of the array
+    for time in np.arange( grid.shape[0] ):
+        ## loop over each gridpoint and get the vertical column
+        for idx, val in np.ndenumerate( grid[0][0] ):
+            ## get the column slice of pressure
             column = surface[ time, :, idx[0], idx[1] ]
+            ## get the column slice of the value to interpolate
             column_GRID = grid[ time, :, idx[0], idx[1] ]
-            
-            value = numpy.interp( interplevels, column, column_GRID, left=numpy.nan, right=numpy.nan )
+            ## interpolate a whole column of valyes and add it to the new array
+            value = np.interp( interplevels, column, column_GRID, left=numpy.nan, right=numpy.nan )
             outgrid[ time, :, idx[0], idx[1] ] = value[:]
     return outgrid
 
