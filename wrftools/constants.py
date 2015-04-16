c40b0b68efcb46a04e695c64ea882d29fddfed68
Kelton Halbert <keltonhalbert@ou.edu>
2015-04-16 08:49:41 -0500

    added meteorological constants

---
+++ wrftools/constants.py
@@ -0,0 +1,8 @@
+"""Frequently used meteorological constants"""
+
+__all__ = ["ZEROCNK", "ROCP", "G", "TOL"]
+
+ZEROCNK = 273.15 # Zero Celsius in Kelvins
+ROCP = 0.28571426 # R over Cp
+G = 9.80665 # Gravity
+TOL = 1e-10 # Floating Point Tolerance
