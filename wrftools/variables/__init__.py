f973e64c650dad4d1ccb78bd5eb4701dfbc37ddd
Kelton Halbert <keltonhalbert@ou.edu>
2015-04-16 15:43:18 -0500

    fixed syntax bug

--- wrftools/variables/__init__.py
+++ wrftools/variables/__init__.py
@@ -1,4 +1,4 @@
 from standard import *
 
-__all__ = ["hght", "pres", "theta", "tmpc", "tmpf"[
+__all__ = ["hght", "pres", "theta", "tmpc", "tmpf"]
 __all__ += ["tmpk", "dwpk", "dwpf", "dwpc", "satvap", "relh"]
