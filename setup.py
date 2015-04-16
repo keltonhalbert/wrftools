9c3ecee9fe5df44b2bc5de0026a9aeb4b7b413f8
Kelton Halbert <keltonhalbert@ou.edu>
2015-04-16 09:30:54 -0500

    updated src path for fortran files

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from numpy.distutils.core import Extension
+from numpy.distutils.core import setup
 
-setup(
-      name = "wrftools",
-      version = "1.5",
-      author = "Kelton Halbert",
-      author_email = "keltonhalbert@ou.edu",
-      description = ("Functions for post processing WRF data"),
-      license = "GPL V2",
-      url = "https://github.com/keltonhalbert/wrftools",
-      packages=['wrftools', 'wrftools.variables', 'wrftools.interp', 'wrftools.utils'],
-      classifiers=["Development Status :: 3 - Alpha"]
-      )
+
+ext1 = Extension(name = "pv_interp",
+    sources=["wrftools/src/interp_pv.f90"])
+
+if __name__ == "__main__":
+    setup(
+          name = "wrftools",
+          version = "2.0",
+          author = "Kelton Halbert",
+          author_email = "keltonhalbert@ou.edu",
+          description = ("Functions for post processing WRF data"),
+          license = "GPL V2",
+          url = "https://github.com/keltonhalbert/wrftools",
+          packages=['wrftools'],
+          classifiers=["Development Status :: 2 - Beta"],
+          ext_modules = [ext1]
+          )
